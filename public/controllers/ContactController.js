477f764bd5bd8950b51810da328881fa445c90fa
Provas Kumar Saha <provasks@gmail.com>
2016-12-11 16:42:39 +0530

    Reformatted the files

--- public/controllers/ContactController.js
+++ public/controllers/ContactController.js
@@ -1,67 +1,64 @@
 /*
-var app = anguler.module('ContactApp',[]);
-app.controller('ContactController',['$scope','$http', function($scope,$http){
-	console.log('Contact controller is working');
-}]);
-*/
-var contactApp = angular.module('contactApp',[]);
+ var app = anguler.module('ContactApp',[]);
+ app.controller('ContactController',['$scope','$http', function($scope,$http){
+ console.log('Contact controller is working');
+ }]);
+ */
+var contactApp = angular.module('contactApp', []);
 
-contactApp.controller('ContactController', ['$scope', '$http', function($scope, $http) {
-  
-  $http.get('/contacts').then(function(resp){
-  	$scope.customers = resp.data;
-  });
+contactApp.controller('ContactController', ['$scope', '$http', function ($scope, $http) {
 
-  $scope.addCustomer = function(){
-  	try{
-   	  $http.post('/contacts',$scope.customer).then(function(resp){
-  	  	$scope.customers.push(resp.data);
-  	  });
-  	}
-  	catch(e){
-  		console.log(e);
-  	}
-  }
-  
-  $scope.editCustomer = function(c){
-    try{
-      var customer = {_id:c._id, name:c.name, email:c.email, contact:c.contact};
-      $scope.customer = customer;
-    }
-    catch(e){
-      console.log(e);
-    }
-  }
+    $http.get('/contacts').then(function (resp) {
+        $scope.customers = resp.data;
+    });
 
-  $scope.updateCustomer = function(){
-    try{
-      $http.put('/contacts', $scope.customer).then(function(resp){
-        var customer =  $scope.customers.filter(function(obj){
-          return obj._id == $scope.customer._id;
-        });
-        customer[0].name = resp.data.name;
-        customer[0].email = resp.data.email;
-        customer[0].contact = resp.data.contact;
-      });
-    }
-    catch(e){
-      console.log(e);
-    }
-  }
+    $scope.addCustomer = function () {
+        try {
+            $http.post('/contacts', $scope.customer).then(function (resp) {
+                $scope.customers.push(resp.data);
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    $scope.editCustomer = function (c) {
+        try {
+            var customer = {_id: c._id, name: c.name, email: c.email, contact: c.contact};
+            $scope.customer = customer;
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    $scope.updateCustomer = function () {
+        try {
+            $http.put('/contacts', $scope.customer).then(function (resp) {
+                var customer = $scope.customers.filter(function (obj) {
+                    return obj._id === $scope.customer._id;
+                });
+                customer[0].name = resp.data.name;
+                customer[0].email = resp.data.email;
+                customer[0].contact = resp.data.contact;
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
 
-  $scope.removeCustomer = function(id){
-    try{
-      $http.delete('/contacts/'+id).then(function(resp){
-        $scope.customers =  $scope.customers.filter(function(obj){
-          return obj._id !=id;
-        });
-      });
-    }
-    catch(e){
-      console.log(e);
-    }
-  }
+    $scope.removeCustomer = function (id) {
+        try {
+            if (confirm('Sure Remove?'))
+                $http.delete('/contacts/' + id).then(function (resp) {
+                    $scope.customers = $scope.customers.filter(function (obj) {
+                        return obj._id !== id;
+                    });
+                });
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
 }]);
 
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var path = require('path');
 var mongojs = require('mongojs');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
-var db = mongojs('contactlist',['contactlist']);
+var db = mongojs('contactlist', ['contactlist']);
 
 
 var app = express();
@@ -13,49 +13,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
 
-
-
-
-app.get('/contacts',function(req, res){
-	db.contactlist.find(function(err, result){
-		res.json(result);
-	});
+app.get('/contacts', function (req, res) {
+    db.contactlist.find(function (err, result) {
+        res.json(result);
+    });
 });
 
-app.post('/contacts',function(req, res){
-	db.contactlist.insert(req.body, function(err, result){
-		res.json(result);
-	});
+app.post('/contacts', function (req, res) {
+    db.contactlist.insert(req.body, function (err, result) {
+        res.json(result);
+    });
 });
 
-// app.put('/contacts/:id',function(req, res){
-//     db.contactlist.findAndModify({
-//         query:{_id:mongojs.ObjectId(req.params.id)}, 
-//         update:{$set:{name:req.body.name, email:req.body.email, contact:req.body.contact}}.
-//         new:true
-//     }, function(err, result){
-//         res.json(result);
-//     });
-// });
-
-app.put('/contacts',function(req, res){
+app.put('/contacts', function (req, res) {
     db.contactlist.findAndModify({
-        query:{_id:mongojs.ObjectId(req.body._id)}, 
-        update:{$set:{name:req.body.name, email:req.body.email, contact:req.body.contact}},
-        new:true
-    },function(err, result){
+        query: {_id: mongojs.ObjectId(req.body._id)},
+        update: {$set: {name: req.body.name, email: req.body.email, contact: req.body.contact}},
+        new : true
+    }, function (err, result) {
         res.json(result);
     });
 });
 
-
-app.delete('/contacts/:id',function(req, res){
-    db.contactlist.remove({_id:mongojs.ObjectId(req.params.id)}, function(err, result){
+app.delete('/contacts/:id', function (req, res) {
+    db.contactlist.remove({_id: mongojs.ObjectId(req.params.id)}, function (err, result) {
         res.json(result);
     });
 });
 
-
-app.listen(3000, function(){
-	console.log("Node Server started on port 3000 ......");
+app.listen(3000, function () {
+    console.log("Node Server started on port 3000 ......");
 });
